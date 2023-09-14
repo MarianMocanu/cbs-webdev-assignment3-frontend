32053910a8a44d6f0b1d479dbc54942469f61b55
Marian <mocanumarian1993@gmail.com>
2023-09-14 19:40:58 +0200

    assets and html

--- create-travel-destination.js
+++ create-travel-destination.js
@@ -1,3 +1,64 @@
+const form = document.getElementById("travel-destination-form");
+
+const title = document.getElementById("title");
+const link = document.getElementById("link");
+const arrivalDate = document.getElementById("arrival-date");
+const departureDate = document.getElementById("departure-date");
+const image = document.getElementById("image");
+const description = document.getElementById("description");
+const country = document.getElementById("country");
+
+const imageToBase64 = (file) => {
+  return new Promise((resolve, reject) => {
+    const reader = new FileReader();
+    reader.readAsDataURL(file);
+    reader.onload = () => resolve(reader.result);
+    reader.onerror = (error) => reject(error);
+  });
+};
+
+form.addEventListener("submit", async (event) => {
+  event.preventDefault();
+  if (!country.value) {
+    // TODO Feedback for user
+    console.log("Country is required");
+    return;
+  }
+  if (!title.value) {
+    // TODO Feedback for user
+    console.log("Title is required");
+    return;
+  }
+  // TODO handling of empty dates and image
+
+  const response = await createTravelDestination({
+    country: country.value,
+    title: title.value,
+    link: link.value ? link.value : undefined,
+    arrivalDate: arrivalDate.value ? new Date(arrivalDate.value).toISOString() : undefined,
+    departureDate: departureDate.value ? new Date(departureDate.value).toISOString() : undefined,
+    image: image.files[0] ? await imageToBase64(image.files[0]) : undefined,
+    description: description.value ? description.value : undefined,
+  });
+  if (response.ok) {
+    const body = await response.json();
+    // TODO update UI / give feedback
+    console.log(body);
+    closeFormPage();
+  }
+});
+
+const createTravelDestination = async (travelDestination) => {
+  const response = await fetch("http://localhost:3000/travel-destination", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(travelDestination),
+  });
+  return response;
+};
+
 const closeFormPage = () => {
   window.location.href = "view-travel-destinations.html";
 };
--- view-travel-destinations.js
+++ view-travel-destinations.js
@@ -1,3 +1,89 @@
 const openFormPage = () => {
   window.location.href = "create-travel-destination.html";
 };
+
+document.addEventListener("DOMContentLoaded", async () => {
+  const response = await fetchTravelDestinations();
+  let travelDestinations;
+  if (response.ok) {
+    travelDestinations = await response.json();
+    updateUI(travelDestinations);
+  }
+});
+
+const fetchTravelDestinations = async () => {
+  const response = await fetch("http://localhost:3000/travel-destinations");
+  return response;
+};
+
+const updateUI = (destinations) => {
+  const destinationsContainer = document.getElementById("destinations-container");
+  const htmlDestinations = convertToHTML(destinations);
+  destinationsContainer.append(...htmlDestinations);
+};
+
+const convertToHTML = (destinations) => {
+  const htmlDestinations = destinations.map((destination) => {
+    const destinationContainer = document.createElement("div");
+    destinationContainer.classList.add("destination-container");
+
+    const image = document.createElement("img");
+    image.classList.add("td-image");
+    if (destination.image) {
+      image.src = destination.image;
+    } else {
+      image.src = "assets/no-image-available.png";
+    }
+
+    const detailsContainer = document.createElement("div");
+    detailsContainer.classList.add("details-container");
+
+    const header = document.createElement("div");
+    header.classList.add("td-header");
+
+    const path = document.createElement("img");
+    path.classList.add("path");
+    path.src = "assets/path.svg";
+
+    const country = document.createElement("span");
+    country.classList.add("country");
+    country.textContent = destination.country;
+
+    const link = document.createElement("a");
+    link.classList.add("td-link");
+    if (destination.link) {
+      link.href = destination.link;
+      link.target = "_blank";
+      link.textContent = "View on Google Maps";
+    }
+
+    header.append(path, country, link);
+
+    const title = document.createElement("div");
+    title.classList.add("td-title");
+    title.textContent = destination.title;
+
+    const date = document.createElement("div");
+    date.classList.add("td-date");
+    if (destination.arrivalDate && destination.departureDate) {
+      date.textContent = `${formatDate(destination.arrivalDate)} - ${formatDate(destination.departureDate)}`;
+    }
+
+    const description = document.createElement("div");
+    description.classList.add("td-description");
+    description.textContent = destination.description;
+
+    detailsContainer.append(header, title, date, description);
+
+    destinationContainer.append(image, detailsContainer);
+
+    return destinationContainer;
+  });
+  return htmlDestinations;
+};
+
+const formatDate = (isoString) => {
+  const date = new Date(isoString);
+  const months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
+  return `${date.getDate()} ${months[date.getMonth()]}, ${date.getFullYear()}`;
+};
